e6f3b5f08de52f3f6cb3f82f01913320d545be1d
alexpxrad <alexpxrad@gmail.com>
2022-10-14 22:58:15 -0400

    changed pictures of nfts

--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const MemoryToken = artifacts.require('./MemoryToken.sol')
 
 require('chai')
@@ -5,5 +7,31 @@ require('chai')
   .should()
 
 contract('Memory Token', (accounts) => {
-  // code goes here...
+  let token 
+
+  before(async () => {
+    token = await MemoryToken.deployed()
+  })
+
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      token = await MemoryToken.deployed()
+      const address = token.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+
+    it('has a name', async () => {
+      const name = await token.name()
+      assert.equal(name, 'Memory Token')
+    })
+    it('has a symbol', async () => {
+      const symbol = await token.symbol()
+      assert.equal(symbol, 'MEMORY')
+    })
+  })
 })
+
+
